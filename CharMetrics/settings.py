6712c2be289dc5e53095b1dbfccf85eb048f2964
svdC1 <svdC1mail@gmail.com>
2024-06-25 19:27:56 -0300

    Update settings.py

--- CharMetrics/settings.py
+++ CharMetrics/settings.py
@@ -12,12 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import mysql
 from dotenv import load_dotenv
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
