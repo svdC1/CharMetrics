4c348e73f88102d35df58c225056155849926d1d
svdC1 <svdC1mail@gmail.com>
2024-06-26 17:06:45 -0300

    Delete main directory

--- main/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class CreatorHandleForm(forms.Form):
-    creators_handle = forms.CharField(label='',max_length=100,widget=forms.TextInput(attrs={'placeholder':"Handle Without '@'","id":'creators_handle'}))
--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home,get_task_result
-
-urlpatterns = [
-    path('',home, name='home'),
-    path('get_task_result/<str:task_id>/', get_task_result, name='get_task_result'),
-]
