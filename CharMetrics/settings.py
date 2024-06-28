4673cc45ad46ea20531eec9c147a8adc528141b8
svdC1 <svdC1mail@gmail.com>
2024-06-28 03:35:36 -0300

    Update settings.py

--- CharMetrics/settings.py
+++ CharMetrics/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
+        'PORT':'3306',
     }
 }
 
